eead3987bc3a11f829ce6744e2ddd5ebb6afc5b6
ArtXs <arti06tube@gmail.com>
2023-10-08 20:27:58 +0400

    Reformat change

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static AtomicInteger for3 = new AtomicInteger();
     public static AtomicInteger for4 = new AtomicInteger();
     public static AtomicInteger for5 = new AtomicInteger();
+
     public static void main(String[] args) throws InterruptedException {
         Random random = new Random();
         String[] texts = new String[100_000];
@@ -13,7 +14,7 @@ public class Main {
         }
 
         Thread palindrome = new Thread(() -> {
-            for (String text: texts) {
+            for (String text : texts) {
                 if (isPolindrome(text) && isSameChar(text)) {
                     incrementCounter(text.length());
                 }
@@ -22,7 +23,7 @@ public class Main {
         palindrome.start();
 
         Thread sameChar = new Thread(() -> {
-            for(String text : texts) {
+            for (String text : texts) {
                 if (isSameChar(text)) {
                     incrementCounter(text.length());
                 }
@@ -31,8 +32,8 @@ public class Main {
         sameChar.start();
 
         Thread ascending = new Thread(() -> {
-            for(String text : texts) {
-                if(!isSameChar(text) && isAscendingOrder(text)) {
+            for (String text : texts) {
+                if (!isSameChar(text) && isAscendingOrder(text)) {
                     incrementCounter(text.length());
                 }
             }
@@ -47,6 +48,7 @@ public class Main {
         System.out.println("Красивых слов с длинной 4: " + for4);
         System.out.println("Красивых слов с длинной 5: " + for5);
     }
+
     public static String generateText(String letters, int length) {
         Random random = new Random();
         StringBuilder text = new StringBuilder();
@@ -62,7 +64,7 @@ public class Main {
 
     public static boolean isSameChar(String text) {
         for (int i = 1; i < text.length(); i++) {
-            if(text.charAt(i) != text.charAt(i - 1)) {
+            if (text.charAt(i) != text.charAt(i - 1)) {
                 return false;
             }
         }
@@ -70,8 +72,8 @@ public class Main {
     }
 
     public static boolean isAscendingOrder(String text) {
-        for (int i = 1; i<text.length(); i++) {
-            if(text.charAt(i) < text.charAt(i - 1)) {
+        for (int i = 1; i < text.length(); i++) {
+            if (text.charAt(i) < text.charAt(i - 1)) {
                 return false;
             }
         }
@@ -79,9 +81,9 @@ public class Main {
     }
 
     public static void incrementCounter(int textLength) {
-        if(textLength == 3) {
+        if (textLength == 3) {
             for3.getAndIncrement();
-        }else if(textLength == 4) {
+        } else if (textLength == 4) {
             for4.getAndIncrement();
         } else {
             for5.getAndIncrement();
